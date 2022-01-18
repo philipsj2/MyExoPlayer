e83d7e88b7ec4fd95a5fcb2343951912db1e16bd
Philips J <philipsj2@outlook.com>
2022-01-18 00:22:22 -0800

    Third Commit

--- app/src/main/java/com/example/myexoplayer/MainActivity.java
+++ app/src/main/java/com/example/myexoplayer/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.myexoplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.ui.DefaultTrackNameProvider;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
-import com.google.android.exoplayer2.ui.TrackNameProvider;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
@@ -24,22 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private static final String VIDEO_URI =
             "https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4";
 
-    private static MainActivity _currentInstance;
+    private final Player.Listener PLAYBACK_STATE_LISTENER = new PlaybackStateListener();
 
     private ExoPlayer _player;
-    private StyledPlayerView _playerView;
-
-    private Player.Listener _playbackStateListener = new PlaybackStateListener();
-    private BandwidthMeter _bandwidthMeter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        _currentInstance = this;
-
-        _playerView = findViewById(R.id.styledPlayerView_playerView);
 
         try {
             _player = new ExoPlayer
@@ -47,16 +35,18 @@ public class MainActivity extends AppCompatActivity {
                     .setLooper(Looper.getMainLooper())
                     .build();
 
-            _playerView.setPlayer(_player);
+            StyledPlayerView playerView = findViewById(R.id.styledPlayerView_playerView);
+            playerView.setPlayer(_player);
 
-            _player.addListener(_playbackStateListener);
+            _player.addListener(PLAYBACK_STATE_LISTENER);
             _player.setMediaItem(MediaItem.fromUri(VIDEO_URI));
             _player.prepare();
             _player.setPlayWhenReady(true);
 
             Handler handler = new Handler();
-            _bandwidthMeter = new DefaultBandwidthMeter.Builder(MainActivity.this).build();
-            _bandwidthMeter.addEventListener(
+            BandwidthMeter bandwidthMeter =
+                    new DefaultBandwidthMeter.Builder(MainActivity.this).build();
+            bandwidthMeter.addEventListener(
                     handler,
                     (elapsedMs, bytesTransferred, bitrateEstimate) -> {
                         Log.d(TAG, "Average bitrate (bps) = " +
@@ -72,18 +62,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        _player.removeListener(_playbackStateListener);
+        _player.removeListener(PLAYBACK_STATE_LISTENER);
         _player.release();
-    }
-
-    private static MainActivity getInstance() {
-        return _currentInstance;
-    }
-
-    private String getBitrate() {
-        Format format = _player.getVideoFormat();
-        TrackNameProvider trackNameProvider = new DefaultTrackNameProvider(getResources());
-        return trackNameProvider.getTrackName(format);
     }
 
     private static class PlaybackStateListener implements Player.Listener {
@@ -104,13 +84,6 @@ public class MainActivity extends AppCompatActivity {
                         );
 
                 Log.d(TAG, "Current player state: " + state);
-                Toast.makeText(MainActivity.getInstance(), state, Toast.LENGTH_LONG).show();
-
-                /*String bitrate = MainActivity.getInstance().getBitrate();
-                Log.d(TAG, "Bitrate: " + bitrate);
-                Toast.makeText(
-                        MainActivity.getInstance(), "Bitrate: " + bitrate, Toast.LENGTH_LONG)
-                        .show();*/
             } catch (Exception e) {
                 Log.e(TAG, e.getMessage());
             }
